0551807c80cb1f1d8e2437510584ab1799520200
oederson <eder.hu@live.com.pt>
2024-01-10 03:44:35 -0300

    Corrido erro basico

--- Api.NET8/Program.cs
+++ Api.NET8/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddAuthorization(opts =>
 {
     opts.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
     opts.AddPolicy("User", policy => policy.RequireRole("User"));
+    opts.AddPolicy("Anonimo", policy => policy.RequireRole("Anonimo"));
 });
 
 var app = builder.Build();
@@ -34,5 +35,6 @@ app.UseAuthorization();
 app.MapGet("/", [Authorize]() => "Hello World!");
 app.MapGet("/testeRoleUser", [Authorize] () => "Funcionou").RequireAuthorization("User");
 app.MapGet("/testeRoleAdmin", [Authorize] () => "Funcionou").RequireAuthorization("Admin");
+app.MapGet("/testeRoleAnonimo", [Authorize] () => "Funcionou").RequireAuthorization("Anonimo");
 
 app.Run();
